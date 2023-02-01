69ee3038ea00c132d7dfe07d3491ed75512cc84c
Jan Kneissler <jan.kneissler@breuninger.de>
2023-02-01 20:18:29 +0100

    START STEP with wildcard

--- src/main/java/org/jkube/gitbeaver/base/command/StepStartCommand.java
+++ src/main/java/org/jkube/gitbeaver/base/command/StepStartCommand.java
@@ -6,18 +6,18 @@ import org.jkube.gitbeaver.WorkSpace;
 
 import java.util.Map;
 
-public class StepStartCommand extends AbstractCommand {
+import static org.jkube.gitbeaver.CommandParser.REST;
 
-    private static final String TITLE = "title";
+public class StepStartCommand extends AbstractCommand {
 
     public StepStartCommand() {
         super("Marks the start of a step block (for structured logging)");
-        commandline("STEP START "+TITLE);
-        argument(TITLE, "the name of the step (used as title of the step section in the log)");
+        commandline("STEP START *");
+        argument(REST, "String to be used as title of the step section in the log");
     }
 
     @Override
     public void execute(Map<String, String> variables, WorkSpace workSpace, Map<String, String> arguments) {
-        GitBeaver.getApplicationLogger(variables).createStep(arguments.get(TITLE));
+        GitBeaver.getApplicationLogger(variables).createStep(arguments.get(REST));
     }
 }
