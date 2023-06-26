0bbfdedd1ebe1fc4aa5484d562db1e3e9d9d20af
Jan Kneissler <j@kneissler.com>
2023-06-26 10:34:13 +0200

    Add set removing command

--- src/main/java/org/jkube/gitbeaver/base/command/SetRemovingCommand.java
+++ src/main/java/org/jkube/gitbeaver/base/command/SetRemovingCommand.java
@@ -7,24 +7,22 @@ import java.util.Map;
 
 import static org.jkube.gitbeaver.CommandParser.REST;
 
-public class SetSubstitutedCommand extends AbstractCommand {
+public class SetRemovingCommand extends AbstractCommand {
 
     private static final String REGEX = "regex";
-    private static final String SUBSTITUTE = "substitute";
     private static final String VARIABLE = "variable";
 
-    public SetSubstitutedCommand() {
+    public SetRemovingCommand() {
         super("Set a variable to a value after applying a regex substitution");
-        commandline("SET "+VARIABLE+" SUBSTITUTING "+REGEX+" BY "+SUBSTITUTE+" IN *");
+        commandline("SET "+VARIABLE+" REMOVING "+REGEX+" IN *");
         argument(VARIABLE, "name of the variable to be set");
-        argument(REGEX, "regular expression to be replaced");
-        argument(SUBSTITUTE, "string to be prelaces found places with");
+        argument(REGEX, "regular expression to be removed");
         argument(REST, "input string");
     }
 
     @Override
     public void execute(Map<String, String> variables, WorkSpace workSpace, Map<String, String> arguments) {
-        variables.put(arguments.get(VARIABLE), arguments.get(REST).replaceAll(arguments.get(REGEX), arguments.get(SUBSTITUTE)));
+        variables.put(arguments.get(VARIABLE), arguments.get(REST).replaceAll(arguments.get(REGEX), ""));
     }
 
 }
