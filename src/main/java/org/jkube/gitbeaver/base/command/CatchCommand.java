1ddbb6f74195750511e2ae12f423ff422223fe0e
Jan Kneissler <j@kneissler.com>
2023-07-12 07:45:35 +0200

    CatchCommand logs exception and stacktrace

--- src/main/java/org/jkube/gitbeaver/base/command/CatchCommand.java
+++ src/main/java/org/jkube/gitbeaver/base/command/CatchCommand.java
@@ -3,7 +3,11 @@ package org.jkube.gitbeaver.base.command;
 import org.jkube.gitbeaver.AbstractCommand;
 import org.jkube.gitbeaver.GitBeaver;
 import org.jkube.gitbeaver.WorkSpace;
+import org.jkube.gitbeaver.util.FileUtil;
+import org.jkube.logging.Log;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +18,7 @@ public class CatchCommand extends AbstractCommand {
     private static final String SCRIPT = "script";
 
     public CatchCommand() {
-        super("Catch exceptions when executing a command");
+        super("Catch exceptions when executing a command, stacktrace and message will be written to files 'stacktrace.txt' and 'exception.txt' in current working directory");
         commandline("CATCH IN "+SCRIPT+" DO "+REST);
         argument(SCRIPT, "the script to be executed when an exception occurs");
         argument(REST, "the command to be tried (from which occuring exceptions will be caught)");
@@ -30,8 +34,17 @@ public class CatchCommand extends AbstractCommand {
                     .parseCommand(arguments.get(REST), parsedArgs)
                     .execute(variables, workSpace, parsedArgs);
         } catch (Throwable t) {
+            Log.warn("Exception was caught: "+t.getMessage());
+            FileUtil.store(workSpace.getAbsolutePath("exception.txt"), t.getMessage());
+            FileUtil.store(workSpace.getAbsolutePath("stacktrace.txt"), getStackTrace(t));
             GitBeaver.scriptExecutor().executeNotSharingVariables(script, null, variables, workSpace, workSpace);
         }
+    }
+
+    private String getStackTrace(Throwable t) {
+        StringWriter stringWriter = new StringWriter();
+        t.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
     }
 
 }
