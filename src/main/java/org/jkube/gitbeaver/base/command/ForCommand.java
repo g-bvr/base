f1f64c2d82788bed28afe539177ed6cf7730ecfc
Jan Kneissler <jan.kneissler@breuninger.de>
2025-05-02 18:30:44 +0200

    add for variant looping of split substrings

--- src/main/java/org/jkube/gitbeaver/base/command/ForCommand.java
+++ src/main/java/org/jkube/gitbeaver/base/command/ForCommand.java
@@ -3,6 +3,7 @@ package org.jkube.gitbeaver.base.command;
 import org.jkube.gitbeaver.AbstractCommand;
 import org.jkube.gitbeaver.GitBeaver;
 import org.jkube.gitbeaver.WorkSpace;
+import org.jkube.gitbeaver.application.Application;
 import org.jkube.gitbeaver.interfaces.Command;
 import org.jkube.gitbeaver.util.FileUtil;
 import org.jkube.gitbeaver.util.VariableSubstitution;
@@ -20,16 +21,19 @@ import static org.jkube.gitbeaver.CommandParser.REST;
 public class ForCommand extends AbstractCommand {
 
     public enum ForType {
-        FILE, FOLDER, SUBFOLDER, LINE
+        FILE, FOLDER, SUBFOLDER, LINE, SUBSTRING
     }
 
     private static final String FOLDERVAR = "foldervar";
     private static final String SUBFOLDERVAR = "subfoldervar";
     private static final String FILEVAR = "filevar";
     private static final String LINEVAR = "linevar";
+    private static final String SUBSTRINGVAR = "substringvar";
     private static final String FILE = "file";
     private static final String FOLDER = "folder";
     private static final String PATTERN = "pattern";
+    private static final String STRING = "string";
+    private static final String SEPARATOR = "separator";
     private static final String DO = "DO"; // shortcut for EXECUTE, with extra logging the for item
 
     public ForCommand() {
@@ -42,6 +46,7 @@ public class ForCommand extends AbstractCommand {
         commandlineVariant("FOR FOLDER "+FOLDERVAR+" IN "+FOLDER+" "+REST, "Execute the command for all subfolders of specified folder");
         commandlineVariant("FOR SUBFOLDER "+SUBFOLDERVAR+" IN "+FOLDER+" "+REST, "Execute the command for all subfolders of specified folder");
         commandlineVariant("FOR LINE "+LINEVAR+" IN "+FILE+" "+REST, "Execute the command for all lines of specified text file");
+        commandlineVariant("FOR SUBSTRING "+SUBSTRINGVAR+" IN "+STRING+" SPLIT BY "+SEPARATOR+" "+REST, "Execute the command for all comma separated pieces of specified string");
         argument(FOLDERVAR, "the variable into the folder item (its path relative to the workspace) shall be written");
         argument(SUBFOLDERVAR, "the variable into the folder item (its path relative containing folder) shall be written");
         argument(FILEVAR, "the variable into the file item (its path relative to the workspace) shall be written");
@@ -49,14 +54,19 @@ public class ForCommand extends AbstractCommand {
         argument(FILE, "the file from which lines shall be read");
         argument(FOLDER, "the folder in which files/subfolders shall be listed");
         argument(PATTERN, "a regex expression by which the found items are filtered, the wildcard character # is resolved to [0-9]+ i.e. matches numbers");
+        argument(SUBSTRINGVAR, "the loop variable holding the current substring");
+        argument(STRING, "the string value that shall be split into substrings to loop over");
+        argument(SEPARATOR, "the string value that will serve as separator in splitting (e.g. ',')");
         argument(REST, "can be either 'DO script' to execute a script or any other command");
     }
 
     @Override
     public void execute(Map<String, String> variables, WorkSpace workSpace, Map<String, String> arguments) {
         String variable;
-        String filename;
         ForType type;
+        String filename = null;
+        String stringvalue = null;
+        String separatorvalue = null;
         if (arguments.containsKey(LINEVAR)) {
             variable = arguments.get(LINEVAR);
             filename = arguments.get(FILE);
@@ -69,10 +79,19 @@ public class ForCommand extends AbstractCommand {
             variable = arguments.get(SUBFOLDERVAR);
             filename = arguments.get(FOLDER);
             type = ForType.SUBFOLDER;
-        } else {
+        } else if (arguments.containsKey(FILEVAR)) {
             variable = arguments.get(FILEVAR);
             filename = arguments.get(FOLDER);
+            type = ForType.SUBFOLDER;
+        } else if (arguments.containsKey(SUBSTRINGVAR)) {
+            variable = arguments.get(SUBSTRINGVAR);
+            stringvalue = arguments.get(STRING);
+            separatorvalue = arguments.get(SEPARATOR);
             type = ForType.FILE;
+        } else {
+            variable = null;
+            type = null;
+            Application.fail("Unexpected case");
         }
         String pattern = arguments.get(PATTERN);
         if (pattern == null) {
@@ -86,6 +105,7 @@ public class ForCommand extends AbstractCommand {
             case FILE -> readItemsInFolder(file, regex, false, workSpace);
             case FOLDER -> readItemsInFolder(file, regex, true, workSpace);
             case SUBFOLDER -> readItemsInFolder(file, regex, true, null);
+            case SUBSTRING -> splitItemsInString(stringvalue, separatorvalue);
         };
         String rest = arguments.get(REST);
         Expect.notNull(rest).elseFail("no command to be executed was specified");
@@ -113,6 +133,10 @@ public class ForCommand extends AbstractCommand {
         } else {
             variables.put(variable, prevValue);
         }
+    }
+
+    private List<String> splitItemsInString(String stringvalue, String separatorvalue) {
+        return List.of(stringvalue.split(separatorvalue));
     }
 
     private List<String> readLines(File file, Pattern regex) {
